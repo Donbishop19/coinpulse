48fc897c849130f4bf0671b7a35aee5e8463802d
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-04-23 01:41:40 -0700

    implement coin details real-time page

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -1,11 +1,12 @@
 'use server';
+
 import qs from 'query-string';
 
 const BASE_URL = process.env.COINGECKO_BASE_URL;
 const API_KEY = process.env.COINGECKO_API_KEY;
 
-if (!BASE_URL) throw new Error('could not get base url');
-if (!API_KEY) throw new Error('could not get api key');
+if (!BASE_URL) throw new Error('Could not get base url');
+if (!API_KEY) throw new Error('Could not get api key');
 
 export async function fetcher<T>(
   endpoint: string,
@@ -22,7 +23,7 @@ export async function fetcher<T>(
 
   const response = await fetch(url, {
     headers: {
-      'x-cg-demo-api-key': API_KEY,
+      'x-cg-pro-api-key': API_KEY,
       'Content-Type': 'application/json',
     } as Record<string, string>,
     next: { revalidate },
@@ -31,8 +32,42 @@ export async function fetcher<T>(
   if (!response.ok) {
     const errorBody: CoinGeckoErrorBody = await response.json().catch(() => ({}));
 
-   throw new Error(`API Error: ${response.status}: ${errorBody.error || response.statusText}`);
+    throw new Error(`API Error: ${response.status}: ${errorBody.error || response.statusText} `);
   }
 
   return response.json();
+}
+
+export async function getPools(
+  id: string,
+  network?: string | null,
+  contractAddress?: string | null,
+): Promise<PoolData> {
+  const fallback: PoolData = {
+    id: '',
+    address: '',
+    name: '',
+    network: '',
+  };
+
+  if (network && contractAddress) {
+    try {
+      const poolData = await fetcher<{ data: PoolData[] }>(
+        `/onchain/networks/${network}/tokens/${contractAddress}/pools`,
+      );
+
+      return poolData.data?.[0] ?? fallback;
+    } catch (error) {
+      console.log(error);
+      return fallback;
+    }
+  }
+
+  try {
+    const poolData = await fetcher<{ data: PoolData[] }>('/onchain/search/pools', { query: id });
+
+    return poolData.data?.[0] ?? fallback;
+  } catch {
+    return fallback;
+  }
 }
